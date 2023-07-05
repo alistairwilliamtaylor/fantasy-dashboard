f0ef5047bd97c0c5f6ba382d9c748ea147e9857b
joshua-hui <joshua.hui@rea-group.com>
2023-07-05 22:30:27 +1000

    add previous agm proposals

--- src/AGM/linksAGM.js
+++ src/AGM/linksAGM.js
@@ -1,7 +1,15 @@
 const links = [
     {
-        label:"AGM proposals",
+        label:"AGM proposals 2023",
         href:'https://docs.google.com/spreadsheets/d/1IyPR-2wBNFr-JoTLk2HOzV8UeoHqUE9XEXwD3BMBmSA',
+    },
+    {
+        label:"AGM proposals 2022",
+        href:'https://docs.google.com/spreadsheets/d/1IyPR-2wBNFr-JoTLk2HOzV8UeoHqUE9XEXwD3BMBmSA/edit#gid=0',
+    },
+    {
+        label:"AGM proposals 2021",
+        href:'https://docs.google.com/spreadsheets/d/105uoTifmmSFjq2B-Uu3GUkJk8uCg0ruyql5vh0XgS-U/edit#gid=0',
     },
 ]
 
