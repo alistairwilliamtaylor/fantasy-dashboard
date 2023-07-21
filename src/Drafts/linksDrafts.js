c20e5934120fa779168ef2c049160d016a267a3b
juffleberry <51109381+juffleberry@users.noreply.github.com>
2023-07-21 22:02:35 +1000

    Update linksDrafts.js
    add 2023 draft board

--- src/Drafts/linksDrafts.js
+++ src/Drafts/linksDrafts.js
@@ -1,24 +1,28 @@
 const links = [
     {
-        label: "2018 Draft",
-        href: "https://docs.google.com/spreadsheets/d/1oT82SD2BRACehVheg50eoCtPrqVJvmOavkyx0jIrhOQ/edit#gid=0",
+        label: "2023 Draft",
+        href: "https://docs.google.com/spreadsheets/d/1mmyhPBC18M2yO2zjLB6ETkq0tBSDCYiWYLKw3icbxKE/edit#gid=0",
     },
     {
-        label: "2019 Draft",
-        href: "https://docs.google.com/spreadsheets/d/1zTpePX16KusHIO-YRCcweyX6-gICwlqUQE2ynC0ELr4/edit#gid=0",
-    },
-    {
-        label: "2020 Draft",
-        href: "https://docs.google.com/spreadsheets/d/1xkzfDaGXhi5xF3Bo07OaHEywqARYN2g7ndybHgOr8SI/edit#gid=0",
+        label: "2022 Draft",
+        href: "https://docs.google.com/spreadsheets/d/1fO9Ix1G3ASH3wclKkIC0a6uj2tvZMxtJQRu8vn_NrG8/edit#gid=0",
     },
     {
         label: "2021 Draft",
         href: "https://docs.google.com/spreadsheets/d/1QAzK1zoY5LrU_cYH7IT1NlT4h5XKIxQ62M00Yrvhm4E/edit?pli=1#gid=0",
     },
     {
-        label: "2022 Draft",
-        href: "https://docs.google.com/spreadsheets/d/1fO9Ix1G3ASH3wclKkIC0a6uj2tvZMxtJQRu8vn_NrG8/edit#gid=0",
+        label: "2020 Draft",
+        href: "https://docs.google.com/spreadsheets/d/1xkzfDaGXhi5xF3Bo07OaHEywqARYN2g7ndybHgOr8SI/edit#gid=0",
     },
+    {
+        label: "2019 Draft",
+        href: "https://docs.google.com/spreadsheets/d/1zTpePX16KusHIO-YRCcweyX6-gICwlqUQE2ynC0ELr4/edit#gid=0",
+    },
+    {
+        label: "2018 Draft",
+        href: "https://docs.google.com/spreadsheets/d/1oT82SD2BRACehVheg50eoCtPrqVJvmOavkyx0jIrhOQ/edit#gid=0",
+    }
 ];
   
 export default links;
