f3cf7957c3fbbc92bceda923bf9713bb931633f6
Alistair Taylor <alistairwilliamtaylor@gmail.com>
2023-08-05 23:03:28 +1000

    Update CurrentInfo.jsx

--- src/CurrentInfo.jsx
+++ src/CurrentInfo.jsx
@@ -16,12 +16,16 @@ const CurrentInfo = () => (
       </Alert>
       <CardContent>
         <Typography gutterBottom variant="body2" color="text.secondary">
-          Check out the minutes here{" "}
-          <Link href="https://docs.google.com/document/d/1GE61wBqXeEGwYGtLNrAILpO1ZVfz_ynpP1dXufO4yKo/edit?usp=sharing">here</Link>
+          <strong>
+            Check out the minutes{" "}
+            <Link href="https://docs.google.com/document/d/1GE61wBqXeEGwYGtLNrAILpO1ZVfz_ynpP1dXufO4yKo/edit?usp=sharing">here</Link>
+          </strong>  
         </Typography>
         <Typography gutterBottom variant="body2" color="text.secondary">
-          <strong>Womens World Cup Results for draft order {" "}
-          <Link href="https://docs.google.com/spreadsheets/d/17PiF7QvpEOw8xaTWCN4GYLJM0-AB4Lx5qrbcmLySZUE/edit?usp=drive_web&ouid=116300514392742136833">here</Link></strong>
+          <strong>
+            Womens World Cup Results for draft order {" "}
+            <Link href="https://docs.google.com/spreadsheets/d/17PiF7QvpEOw8xaTWCN4GYLJM0-AB4Lx5qrbcmLySZUE/edit?usp=drive_web&ouid=116300514392742136833">here</Link>
+          </strong>
         </Typography>
       </CardContent>
     </Card>
