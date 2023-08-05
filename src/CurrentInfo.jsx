dfde2e5f67b5c0eaa34958e945288d151f302f7e
Alistair Taylor <alistairwilliamtaylor@gmail.com>
2023-08-05 23:01:14 +1000

    Update CurrentInfo.jsx

--- src/CurrentInfo.jsx
+++ src/CurrentInfo.jsx
@@ -12,31 +12,16 @@ const CurrentInfo = () => (
   <Box sx={{ mb: 5 }}>
     <Card sx={{ width: 360 }}>
       <Alert severity="warning">
-        <AlertTitle>Online voting is open!</AlertTitle>
-        <Typography gutterBottom variant="body2" color="text.secondary">
-          Do note vote online if you plan to vote in-person at AGM
-        </Typography>
-        <Typography gutterBottom variant="body2" color="text.secondary">
-          One vote per team - most recent vote will count if multiple owners vote
-        </Typography>
-        <Typography gutterBottom variant="body2" color="text.secondary">
-          You can only vote once, and cannot edit your vote once submitted
-        </Typography>
+        <AlertTitle>AGM minutes are available!</AlertTitle>
       </Alert>
       <CardContent>
         <Typography gutterBottom variant="body2" color="text.secondary">
-          Please vote{" "}
-          <Link href="https://forms.gle/bgPrVfdh8EXiFNnXA">here</Link>
+          Check out the minutes here{" "}
+          <Link href="https://docs.google.com/document/d/1GE61wBqXeEGwYGtLNrAILpO1ZVfz_ynpP1dXufO4yKo/edit?usp=sharing">here</Link>
         </Typography>
         <Typography gutterBottom variant="body2" color="text.secondary">
           <strong>Womens World Cup Results for draft order {" "}
           <Link href="https://docs.google.com/spreadsheets/d/17PiF7QvpEOw8xaTWCN4GYLJM0-AB4Lx5qrbcmLySZUE/edit?usp=drive_web&ouid=116300514392742136833">here</Link></strong>
-        </Typography>
-        <Typography gutterBottom variant="body2" color="text.secondary">
-          <strong>Saturday 22nd July</strong> - Online voting closes
-        </Typography>
-        <Typography gutterBottom variant="body2" color="text.secondary">
-          <strong>Sunday 23rd July 12:00</strong> - AGM at the Courthouse
         </Typography>
       </CardContent>
     </Card>
