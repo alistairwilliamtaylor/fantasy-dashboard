255cbfcd688795ba88ef7e8d31446c81c065fc67
juffleberry <51109381+juffleberry@users.noreply.github.com>
2024-07-09 17:18:01 +1000

    Update CurrentInfo.jsx
    change title card

--- src/CurrentInfo.jsx
+++ src/CurrentInfo.jsx
@@ -17,15 +17,21 @@ const CurrentInfo = () => (
       <CardContent>
         <Typography gutterBottom variant="body2" color="text.secondary">
           <strong>
-            Check out the minutes{" "}
-            <Link href="https://docs.google.com/document/d/1GE61wBqXeEGwYGtLNrAILpO1ZVfz_ynpP1dXufO4yKo/edit?usp=sharing">here</Link>
+            Keeper list for 2024 here{" "}
+            <Link href="https://docs.google.com/spreadsheets/d/1hVJketd8ZBypAq3phyB_GZnVv0AclgTdzR6tJMMeYWg/edit?gid=0#gid=0">here</Link>
           </strong>  
         </Typography>
         <Typography gutterBottom variant="body2" color="text.secondary">
           <strong>
-            Womens World Cup Results for draft order {" "}
-            <Link href="https://docs.google.com/spreadsheets/d/17PiF7QvpEOw8xaTWCN4GYLJM0-AB4Lx5qrbcmLySZUE/edit?usp=drive_web&ouid=116300514392742136833">here</Link>
+            AGM proposals 2024 {" "}
+            <Link href="https://docs.google.com/spreadsheets/d/1IyPR-2wBNFr-JoTLk2HOzV8UeoHqUE9XEXwD3BMBmSA/edit?gid=1092398144#gid=1092398144">here</Link>
           </strong>
+        </Typography>
+        <Typography gutterBottom variant="body2" color="text.secondary">
+          <strong>
+            Updated Constitution (with AGM 2023 tings!){" "}
+            <Link href="https://docs.google.com/document/d/1WTv6wyyhCLDl3IZdLQZoemQmArVeP3whRqmrmD9foHI/edit">here</Link>
+          </strong>  
         </Typography>
       </CardContent>
     </Card>
