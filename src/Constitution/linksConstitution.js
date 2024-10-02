5443048926cd72849e23ad08bf2fd29acfbbb12c
juffleberry <51109381+juffleberry@users.noreply.github.com>
2024-10-02 20:26:16 +1000

    Update linksConstitution.js
    updated constitution link

--- src/Constitution/linksConstitution.js
+++ src/Constitution/linksConstitution.js
@@ -1,7 +1,7 @@
 const links = [
   {
-    label: "Constitution Draft",
-    href: "https://docs.google.com/document/d/1MDXlOvWIK086S4ujMxTszR9PoGzvj_Nw/edit?usp=sharing&ouid=102878776533782093974&rtpof=true&sd=true",
+    label: "Constitution",
+    href: "https://docs.google.com/document/d/1WTv6wyyhCLDl3IZdLQZoemQmArVeP3whRqmrmD9foHI/edit",
   },
 ];
 
