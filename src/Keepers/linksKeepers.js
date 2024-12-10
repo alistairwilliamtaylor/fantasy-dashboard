ab40c52410f1ebff2d235dd18624354dcb8da578
juffleberry <51109381+juffleberry@users.noreply.github.com>
2024-12-10 22:42:24 +1100

    Update linksKeepers.js
    updated keeper list for 2025

--- src/Keepers/linksKeepers.js
+++ src/Keepers/linksKeepers.js
@@ -1,5 +1,9 @@
 const links = [
   {
+    label: "Keeper List 2025",
+    href: "https://docs.google.com/spreadsheets/d/145fLisdZOd6UZsWYNb4Q41e7D_4HhEVXrk7VZKt3epg/edit?gid=0#gid=0",
+  },
+  {
     label: "Keeper List 2024",
     href: "https://docs.google.com/spreadsheets/d/1hVJketd8ZBypAq3phyB_GZnVv0AclgTdzR6tJMMeYWg/edit#gid=0",
   },
